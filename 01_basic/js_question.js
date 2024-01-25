9ce47256cda2602c5f363e5aff21a41f05b64302
visz29 <156419780+visz29@users.noreply.github.com>
2024-01-25 21:16:18 +0530

    qs

--- 01_basic/js_question.js
+++ 01_basic/js_question.js
@@ -1,1 +1,40 @@
+// 1. Sum of Array Elements:
+        // let arr = [1,2,3]
+        // let res = 0
+        // arr.forEach(myfun);
 
+        // function myfun(item,index){
+        //     res = res + item
+        //     console.log(`${index} : ${item}`)
+        // }
+        // console.log(res)
+
+        // 2. Average of Array Element:
+        // let arr = [1,2,5,2]
+        // let res = 0
+        // let avg = 0
+        // console.log(arr.length)
+        // arr.forEach(myfun);
+        
+        // function myfun(item){
+        //     res = res + item;
+        //     avg = res/arr.length
+        // }
+        // console.log(res)
+        // console.log(avg);
+
+        // // 3. Maximum Element:
+        // let arr = [2,3,6,4,5,9]
+        // console.log(arr);
+        // console.log(Math.max(...arr));
+        
+        // // 4. Array length
+        // let arr = ["apple","mango","grapss","pineapple"]
+        // console.log(arr);
+        // console.log(arr.length);
+
+        // 5. Element Existence 
+        // To check if a specific element exists in an array
+        let arr = ["vishal","dipanshu","ghanshaym","varun"]
+        console.log(arr);
+        console.log(arr.includes("vishal"));

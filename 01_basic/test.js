88efd3fc2bce17a64b3148023ebe4025f16e655f
visz29 <156419780+visz29@users.noreply.github.com>
2024-02-06 06:29:53 +0000

    file handling

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,15 @@
 console.log("vishal")
+const fs = require('fs');
+
+// fs.writeFileSync("./my.text","I am vishal");
+
+// fs.appendFileSync("./my.text",", Hey there");
+
+// fs.unlink("./my.text",function (err) {
+//     if (err) throw err;
+//     console.log('File deleted!');
+//   });
+
+// fs.renameSync("my.text","new.txt")
+
+// fs.appendFileSync("./01_basic/my.txt","My name is vshal")
